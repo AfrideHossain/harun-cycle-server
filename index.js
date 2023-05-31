74351eaf93adb52fa8c872fd65bf1edb2df1972e
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2023-05-31 10:45:16 +0600

    auth routes converted from knex sqlite3 to mongodb

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3000;
+const { connectMongo } = require("./mongoConnect");
 
+connectMongo();
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -6,74 +6,70 @@ const fetchValidUser = require("../middleware/fetchvaliduser");
 // import dotenv pack
 require("dotenv").config();
 const jwt_string = process.env.JWT_SECRET;
-const knex = require("../../dbcon");
+const { client } = require("../../mongoConnect");
+const { ObjectId } = require("mongodb");
 
-//ROUTE 1: Log in using POST (/auth/login).
+let adminCollection = client.db("harun_cycle_db").collection("admin_users");
+
+//ROUTE 1-Mongo: Log in using POST (/auth/login).
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   let success = true;
   try {
-    knex("admin")
-      .where("email", email)
-      .select("*")
-      .then((result) => {
-        if (result.length > 0) {
-          let pass = md5(password);
-          if (pass === result[0].password) {
-            let user = {
-              id: result[0].id,
-              username: result[0].username,
-              email: result[0].email,
-            };
-            let data = { user };
-            let authToken = jwt.sign(data, jwt_string);
-            res.json({ success, message: "Login approved", user, authToken });
-          } else {
-            success = false;
-            res.json({ success, message: "Login denied" });
-          }
-        } else {
-          success = false;
-          res.json({
-            success,
-            message: "please try again with correct credentials",
-          });
-        }
+    let cursor = await adminCollection.findOne({ email: email });
+    if (cursor) {
+      let pass = md5(password);
+      if (cursor.password === pass) {
+        let user = {
+          id: cursor._id,
+          username: cursor.username,
+          email: cursor.email,
+        };
+        let data = { user };
+        let authToken = jwt.sign(data, jwt_string);
+        res.json({ success, message: "Login approved", user, authToken });
+      } else {
+        success = false;
+        return res.json({ success, message: "Login denied" });
+      }
+    } else {
+      success = false;
+      return res.json({
+        success,
+        message: "please try again with correct credentials",
       });
+    }
   } catch (error) {
-    res.status(500).send("Internal server error");
+    return res.status(500).send("Internal server error");
   }
 });
 //ROUTE 2: Log in using POST (/auth/userDecp).
 router.post("/userDecp", async (req, res) => {
   const { token } = req.body;
-  // let success = true;
   if (!token) {
     res.status(401).send({ error: "Invalid credentials" });
     return;
   }
   try {
     const data = jwt.verify(token, jwt_string);
-    // req.user = data.user;
-    // console.log(data);
     res.send(data.user);
   } catch (error) {
     res.status(401).send({ error: "Invalid credentials" });
   }
 });
-//ROUTE 2: Log in using POST (/auth/userDecp).
+//ROUTE 2-mongo: password update
 router.post("/updatepass", fetchValidUser, async (req, res) => {
-  const { username, password } = req.body;
-  // let success = true;
-  console.log({ username, password });
+  const id = req.user.id;
+  const pass = md5(req.body.password);
   try {
-    const passUpdate = knex("admin")
-      .where("username", username)
-      .update({ password: md5(password) }, ["uid"]);
-    passUpdate.then((data) => {
-      res.json({ success: true, msg: "Password updated", data });
+    let query = { _id: new ObjectId(id) };
+    let update = { $set: { password: pass } };
+    let updateResult = await adminCollection.updateOne(query, update);
+    res.json({
+      success: true,
+      msg: "Password updated",
+      upResult: updateResult,
     });
-    // res.send(data.user);
   } catch (error) {
     res.status(401).send({ success: false, error: "Invalid credentials" });
   }
